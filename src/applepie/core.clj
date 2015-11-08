e01fa35dadd9dacafcbf51277ac461693c6da35b
Kazuma SATO <ranceworks@gmail.com>
2015-11-09 08:16:26 +0900

    #1 forward / to index.html

--- src/applepie/core.clj
+++ src/applepie/core.clj
@@ -1,19 +1,28 @@
 (ns applepie.core
-  (:require [compojure.route :only [files not-found]]
-            [org.httpkit.server :as kit]
-            [compojure.handler :only [site]]
-            [compojure.core :only [defroutes GET POST DELETE ANY context]]))
+  (:use [compojure.route :only [files not-found files]]
+        [org.httpkit.server :only [run-server]]
+        [compojure.handler :only [site]]
+        [compojure.core :only [defroutes GET POST DELETE ANY context]]
+        [ring.util.response :only [redirect file-response resource-response content-type]]))
 
 
- (defn foo
-   "I don't do a whole lot."
-   [x]
-   (println x "Hello, World!"))
 
+(defroutes all-routes
+           ;           (GET "/" [] (file-response "index.html" {:root "public"}))
+           (GET "/" [] (content-type (resource-response "index.html" {:root "public"}) "text/html"))
 
- (defn app [req]
-   {:status  200
-    :headers {"Content-Type" "text/html"}
-    :body    "hello HTTP!"})
+           (files "/static/")                         ;; static file url prefix /static, in `public` folder
+           (not-found "<p>Page not found.</p>"))      ;; all other, return 404
 
- (kit/run-server app {:port 8080})
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    ;; graceful shutdown: wait 100ms for existing requests to be finished
+    ;; :timeout is optional, when no timeout, stop immediately
+    (@server :timeout 100)
+    (reset! server nil)))
+
+(stop-server)
+(reset! server (run-server (site #'all-routes) {:port 8080}))
+
