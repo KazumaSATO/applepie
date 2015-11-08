e1aff8ff61ea63532a8bea57ee524aa00c5dc226
Kazuma SATO <ranceworks@gmail.com>
2015-11-08 21:48:49 +0900

    resolve ring dependency

--- src/applepie/core.clj
+++ src/applepie/core.clj
@@ -1,6 +1,19 @@
-(ns applepie.core)
+(ns applepie.core
+  (:require [compojure.route :only [files not-found]]
+            [org.httpkit.server :as kit]
+            [compojure.handler :only [site]]
+            [compojure.core :only [defroutes GET POST DELETE ANY context]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+ (defn foo
+   "I don't do a whole lot."
+   [x]
+   (println x "Hello, World!"))
+
+
+ (defn app [req]
+   {:status  200
+    :headers {"Content-Type" "text/html"}
+    :body    "hello HTTP!"})
+
+ (kit/run-server app {:port 8080})
