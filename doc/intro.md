5417ebd6329f509754514d4429671bb4cf947aba
Kazuma SATO <ranceworks@gmail.com>
2015-11-08 20:51:36 +0900

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to applepie
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/applepie/core_test.clj
@@ -0,0 +1,7 @@
+(ns applepie.core-test
+  (:require [clojure.test :refer :all]
+            [applepie.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
