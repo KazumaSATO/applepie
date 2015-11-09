0b8eb608b608de742f100e9062dba0b15bb25025
Kazuma SATO <ranceworks@gmail.com>
2015-11-09 21:12:04 +0900

    #2 make websocket demo

--- src/applepie/core.clj
+++ src/applepie/core.clj
@@ -1,18 +1,31 @@
 (ns applepie.core
   (:use [compojure.route :only [files not-found files]]
-        [org.httpkit.server :only [run-server]]
+        [org.httpkit.timer :only [schedule-task]]
+        [org.httpkit.server :only [run-server with-channel on-close on-receive send!]]
         [compojure.handler :only [site]]
         [compojure.core :only [defroutes GET POST DELETE ANY context]]
         [ring.util.response :only [redirect file-response resource-response content-type]]))
 
-
+(defn log-handler [request]
+  (with-channel request channel
+                (on-close channel (fn [status] (println "channel closed: " status)))
+                (on-receive channel (fn [data] ;; echo it back
+                                      (send! channel data)))
+                (loop []
+                  (Thread/sleep 10000)
+                  (schedule-task 1000 (send! channel "hello!" false)) ; false => don't close after send
+                  (recur))))
 
 (defroutes all-routes
            ;           (GET "/" [] (file-response "index.html" {:root "public"}))
            (GET "/" [] (content-type (resource-response "index.html" {:root "public"}) "text/html"))
-
+           (GET "/ws" [] log-handler)
            (files "/static/")                         ;; static file url prefix /static, in `public` folder
            (not-found "<p>Page not found.</p>"))      ;; all other, return 404
+
+
+
+
 
 (defonce server (atom nil))
 
